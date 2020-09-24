e03f26c86f961ea6571caa2c55ee2113f6cae9ac
Eric Odell <eric.odell@ucop.edu>
2020-09-24 10:37:53 -0700

    output with description

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,24 @@
 output "arn" {
-  value = join("", aws_elasticsearch_domain.this.*.arn)
+  description = "elastic search domain arn"
+  value       = join("", aws_elasticsearch_domain.this.*.arn)
 }
 
 output "domain_id" {
-  value = join("", aws_elasticsearch_domain.this.*.domain_id)
+  description = "elastic search domain id"
+  value       = join("", aws_elasticsearch_domain.this.*.domain_id)
 }
 
 output "domain_name" {
-  value = join("", aws_elasticsearch_domain.this.*.domain_name)
+  description = "elastic search domain name"
+  value       = join("", aws_elasticsearch_domain.this.*.domain_name)
 }
 
 output "endpoint" {
-  value = join("", aws_elasticsearch_domain.this.*.endpoint)
+  description = "elastic search domain endpoint"
+  value       = join("", aws_elasticsearch_domain.this.*.endpoint)
 }
 
 output "kibana_endpoint" {
-  value = join("", aws_elasticsearch_domain.this.*.kibana_endpoint)
+  description = "elastic search domain kibana plugin endpoint"
+  value       = join("", aws_elasticsearch_domain.this.*.kibana_endpoint)
 }
